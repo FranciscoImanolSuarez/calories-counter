9a0b8bff2f1aff732065585698fc1e61d76e8a77
Francisco Imanol Suarez <imanolsuarez98@gmail.com>
2019-05-16 15:25:18 -0300

    Primer commit

---
+++ js/copiarObjetos.js
@@ -0,0 +1,24 @@
+let a = 1
+let b = a
+
+console.log(a, b) // 1 1
+
+b += 1
+
+console.log(a, b)
+
+
+
+/////
+
+//OBJETO DE UN AUTO
+
+let car = {
+    color: 'red',
+    year: 2019,
+
+}
+
+let newCar = car
+
+console.log(car, newCar)
